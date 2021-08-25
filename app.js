ae5325d74c325bebb32d95aa89500f1f0c7f683e
divyansingh <divyansingh.21@gmail.com>
2021-08-25 10:53:17 -0700

    first

---
+++ app.js
@@ -0,0 +1,52 @@
+// const todos = [
+//     {
+//         id: 1,
+//         text: 'Take out trash',
+//         isCompleted: false
+//     },
+//     {
+//         id: 2,
+//         text: 'Take out trash1',
+//         isCompleted: true
+//     },
+//     {
+//         id: 3,
+//         text: 'Take out trash2',
+//         isCompleted: false
+//     },
+//     {
+//         id: 4,
+//         text: 'Take out trash3',
+//         isCompleted: true
+//     },
+// ]
+
+// for(let t of todos){
+//     console.log(t.text)
+// }
+// todos.forEach(function(t){
+//     console.log(t.id)
+// })
+// const todoText=todos.map(function(t){
+//     return t.text
+// })
+// console.log(todoText)
+
+// const todoCompleted = todos.filter(function (t) {
+//     return t.isCompleted === true;
+// }).map(function(t){
+//     return t.text;
+// })
+// console.log(todoCompleted)
+
+
+var a=0.11
+const cc=a>5?'Red':'Blue'
+console.log(cc)
+
+
+
+
+
+
+
